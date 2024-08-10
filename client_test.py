a681503d3648703e3f41bea787eb00d91540596f
kirtan sayta <kirtansayta5503@gmail.com>
2024-08-10 22:37:07 +0530

    updates

--- client_test.py
+++ client_test.py
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +19,34 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_invalidInput(self):
+    with self.assertRaises(TypeError):
+        getDataPoint("Invalid input")
 
+def test_getDataPoint_missingStock(self):
+    quote = {
+        'top_ask': {'price': 121.2, 'size': 36},
+        'timestamp': '2019-02-11 22:06:30.572453',
+        'top_bid': {'price': 120.48, 'size': 109},
+        'id': '0.109974697771'
+    }
+    with self.assertRaises(KeyError):
+        getDataPoint(quote)
 
+def test_getDataPoint_missingTopBid(self):
+    quote = {
+        'top_ask': {'price': 121.2, 'size': 36},
+        'timestamp': '2019-02-11 22:06:30.572453',
+        'stock': 'ABC',
+        'id': '0.109974697771'
+    }
+    with self.assertRaises(KeyError):
+        getDataPoint(quote)
 
 if __name__ == '__main__':
     unittest.main()
